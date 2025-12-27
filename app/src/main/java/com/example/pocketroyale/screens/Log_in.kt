cdcfaa729a3a35720310719abd4add9f4b97a295
ivan-enti <ivan.gonzalez@enti.cat>
2025-12-27 13:47:06 +0100

    Log in updated to backlog style

--- app/src/main/java/com/example/pocketroyale/screens/Log_in.kt
+++ app/src/main/java/com/example/pocketroyale/screens/Log_in.kt
@@ -1,5 +1,7 @@
 package com.example.pocketroyale.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,7 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,30 +26,49 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pocketroyale.R
 import com.example.pocketroyale.enums.ScreenType
 import com.example.pocketroyale.ui.theme.Typography
 import com.example.pocketroyale.components.BackgroundShader
 import com.example.pocketroyale.ui.theme.royaleFont
+import java.time.format.TextStyle
 
 @Composable
 fun LogInScreen(setScreen: (ScreenType) -> Unit, modifier: Modifier = Modifier){
-    Box(modifier = modifier.fillMaxSize().background(BackgroundShader())) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(BackgroundShader())) {
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             //Title
-            Text(
-                text = "Pocket\nRoyale",
-                style = Typography.titleLarge
-            )
-            Spacer(modifier.height(64.dp))
+            Box() {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_royale),
+                    contentDescription = null,
+                    modifier = modifier
+                        .size(256.dp)
+                        .align(Alignment.Center)
+                )
+                Text(
+                    text = "Pocket\nRoyale",
+                    style = Typography.titleLarge,
+                    modifier = modifier.align(Alignment.Center)
+                )
+            }
+            Spacer(modifier.height(16.dp))
 
             //Text Fields
             var username = ""
@@ -50,19 +77,19 @@ fun LogInScreen(setScreen: (ScreenType) -> Unit, modifier: Modifier = Modifier){
             var password_error by remember { mutableStateOf("") }
             Column() {
                 username = LogIn_TextField(
-                    field_name = "username",
+                    field_name = stringResource(R.string.username),
                     hide_value = false,
                     error_message = username_error
                 )
 
                 password = LogIn_TextField(
-                    field_name = "password",
+                    field_name = stringResource(R.string.password),
                     hide_value = true,
                     error_message = password_error
                 )
             }
 
-            Spacer(modifier.height(64.dp))
+            Spacer(modifier.height(16.dp))
 
             //Log in Button
             Button(
@@ -74,9 +101,14 @@ fun LogInScreen(setScreen: (ScreenType) -> Unit, modifier: Modifier = Modifier){
                         setScreen(ScreenType.HOME)
                     }
                 },
+                colors = ButtonDefaults.buttonColors(Color(0xFFFFA301)),
+                border = BorderStroke(4.dp,Color(0xFFBA6304))
             ) {
                 Text(
-                    text = "Log in"
+                    text = "Log in",
+                    style = Typography.bodyLarge,
+                    modifier = modifier.padding(16.dp)
+
                 )
             }
         }
@@ -95,7 +127,8 @@ fun LogIn_TextField(
     //Field name
     Text(
         text = field_name,
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        modifier = modifier.padding(8.dp)
     )
     //Field content
     TextField(
@@ -103,7 +136,8 @@ fun LogIn_TextField(
         onValueChange = {value = it},
         visualTransformation =
             if (hide_value) PasswordVisualTransformation()
-            else VisualTransformation.None
+            else VisualTransformation.None,
+        modifier = modifier.clip(RoundedCornerShape(16.dp))
     )
     //Error message
     Text(
--- app/src/main/java/com/example/pocketroyale/ui/theme/Type.kt
+++ app/src/main/java/com/example/pocketroyale/ui/theme/Type.kt
@@ -1,10 +1,12 @@
 package com.example.pocketroyale.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.example.pocketroyale.R
 
@@ -17,14 +19,17 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
+        color = Color.White
     ),
     titleLarge = TextStyle(
         fontFamily = royaleFont,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.ExtraBold,
         fontSize = 48.sp,
-        lineHeight = 64.sp,
-        letterSpacing = 0.sp
+        lineHeight = 80.sp,
+        letterSpacing = 0.sp,
+        color = Color.White,
+        textAlign = TextAlign.Center
     ),
     labelSmall = TextStyle(
         fontFamily = royaleFont,
